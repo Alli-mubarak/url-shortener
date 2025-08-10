c97504e5c4f5027ec3210b947156ea407d8fa5b6
Alli Mubarak <161517023+Alli-mubarak@users.noreply.github.com>
2025-08-10 20:55:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const dns = require('dns');
+const { hostname } = require('os');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -10,8 +12,18 @@ app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
+
+//this works on all requests
+app.use( function middleware(req, res, next){
+    console.log(req.method + " "+req.path+" - "+ req.ip);
+    next();
+})
+
 //this is for accessing form data sent with POST requests
 app.use(express.urlencoded({ extended: true }));
+
+//creating a temporary memory
+const urlArray = ["lol.com"];
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
@@ -23,8 +35,31 @@ app.get('/api/hello', function(req, res) {
 });
 
 app.post('/api/shorturl',(req, res)=>{
-  res.json({original_url: req.body.url, short_url:11})
-  console.log(req.method, req.hostname, req.ip);
+  const url = req.body.url
+  const hostname = new URL(url).hostname;
+  console.log(hostname, "is being verified for existence");
+  dns.lookup(hostname, (err, address, family)=>{
+    if(err){
+      res.json({error: 'invalid url'});
+    }else{
+  if(urlArray.includes(url)){
+    const urlNumber = urlArray.indexOf(url);
+    res.json({original_url: url, short_url:urlNumber})
+  }else{
+  urlArray.push(url);
+  const urlNumber = urlArray.indexOf(url);
+    res.json({original_url: url, short_url:urlNumber});
+  }  
+  }
+  console.log(hostname, address, family);
+  });
+  console.log(req.method, req.ip, urlArray);
+})
+
+app.get('/api/shorturl/:short_url', (req,res)=>{
+  const shortUrl = req.params.short_url;
+  const originalUrl = urlArray[shortUrl];
+  res.redirect(originalUrl);
 })
 
 app.listen(port, function() {
